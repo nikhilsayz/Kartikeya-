9d3c5ecc8951b52d90b7a80e6dcaf6779977160e
nikhilsayz <100774374+nikhilsayz@users.noreply.github.com>
2022-03-09 10:41:26 +0530

    updated with mean,median,mode.

--- sol4.c
+++ sol4.c
@@ -1,18 +1,46 @@
 #include<stdio.h>
 int main()
 {
-  int i,n,Sum=0,nums;
-  float Avg;
-  printf("\n Please enter the no.of numbers you're going to enter ? : \n");
-  scanf("%d",&n);
-  printf("\nPlease Enter the numbers one by one\n");
-  for(i=0;i<n;++i)
-   {
-     scanf("%d",&nums);
-     Sum = Sum +nums;
-   }
-  Avg = Sum/n;
-  printf("\nSum of the %d Numbers = %d",n, Sum);
-  printf("\nAverage of the %d Numbers = %.2f",n, Avg);
-  return 0;
+int a[2500], n, i, t, j, max=0, c=0, mode=0;
+float s=0, mean, median;
+printf(“Enter how many data you want to input: “);
+scanf(“%d”,&n);
+printf(“Enter %d Data:\n”, n);
+for(i=0; i<n; i++)
+{
+scanf(“%d”,&a[i]);
+s+=a[i];
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+mean = s/(float)n;
+printf(“Mean of given input is:  %.1f\n”,mean);
+if(n%2==0)
+median=( (a[(n-1)/2] + a[(n-1)/2+1] ) / 2.0);
+else
+median=( (a[(n-1)/2]) / 2.0);
+printf(“median of given input is: %.1f\n”, median);
+for(i=0; i<n; i++)
+{
+t=a[i];
+c=0;
+for(j=0; j<n; j++) { if(t==a[j]) c++; if(c>max)
+{
+max=c;
+mode=t;
+}
+}
+}
+printf(“mode of given input is: %d”,mode);
+return 0;
 }
